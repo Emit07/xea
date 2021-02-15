fd787e1600ec8301ed739ee3806fec73f7177e7a
Alessandro De Leo <adv.deleo@gmail.com>
2021-02-15 00:39:21 -0800

    fixed uptime command

--- cogs/info.py
+++ cogs/info.py
@@ -3,18 +3,17 @@ from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Cog
 
-import time
+from datetime import datetime
 
 class info(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.starttime = time.time()
+        self.starttime = print(str(datetime.datetime.now()).split('.')[0])
 
     @commands.command()
     async def uptime(self, ctx):
-        uptime = time.time() - self.starttime()
         embed = discord.Embed(
-            title=f"SlimBot has been up for {uptime}",
+            title=f"SlimBot has been up since {self.starttime}",
             colour=0xe86823
         )
         await ctx.send(embed=embed)
