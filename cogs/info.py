37c811ca0079ece34c97b9a4bc2dd79ae607c2b4
Ale De Leo <aledeleo@Student-adeleo25.hsd1.ca.comcast.net>
2021-02-17 15:01:31 -0800

    more syntax fixes3

--- cogs/info.py
+++ cogs/info.py
@@ -55,7 +55,7 @@ class info(Cog):
         embed.add_field(name="Humans ", value=f"`{len(list(filter(lambda m: not m.bot, ctx.guild.members)))}`", inline=False)
         embed.add_field(name="Bots ", value=f"`{len(list(filter(lambda m: m.bot, ctx.guild.members)))}`", inline=False)
         embed.add_field(name="Bans", value=f"`{len(ctx.guild.bans())}`", inline=False)
-        embed.add_field(name="Status", value=f"`{ ✅ {statuses[0]} 🌝 {statuses[1]} 🔴 {statuses[2]} ⚪ {statuses[3]}}`", inline=False)
+        embed.add_field(name="Status", value=f"`:green_circle: {statuses[0]} :green_circle: {statuses[1]} :red_circle: {statuses[2]} :white_circle: {statuses[3]}`", inline=False)
         embed.add_field(name="Text Channels ", value=f"`{len(ctx.guild.text_channels)}`", inline=False)
         embed.add_field(name="Voice Channels ", value=f"`{len(ctx.guild.voice_channels)}`", inline=False)
         embed.add_field(name="Categories ", value=f"`{len(ctx.guild.categories)}`", inline=False)
