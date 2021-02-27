cf5e93dfdb37057be0cac5c55df9e3a721d54f28
Alessandro De Leo <adv.deleo@gmail.com>
2021-02-27 00:09:28 -0800

    removed bot avatar url, add later there is error

--- cogs/helpcmd.py
+++ cogs/helpcmd.py
@@ -18,8 +18,8 @@ class helpcmd(Cog):
             title="SlimBot Commands",
             colour=0xe86823
         )
-        bot_url=client.user.avatar_url
-        embed.set_thumbnail(url=bot_url)
+        # bot_url=client.user.avatar_url
+        # embed.set_thumbnail(url=bot_url)
         embed.add_field(name="Moderation", value="`?help moderation`", inline=False)
         embed.add_field(name="Fun", value="`?help fun`", inline=False)
         embed.add_field(name="Polls", value="`?help polls`", inline=False)
@@ -33,8 +33,8 @@ class helpcmd(Cog):
             title="SlimBot Moderation Commands",
             colour=0xe86823
         )
-        bot_url=client.user.avatar_url
-        embed.set_thumbnail(url=bot_url)
+        # bot_url=client.user.avatar_url
+        # embed.set_thumbnail(url=bot_url)
         embed.add_field(name="`?mute [user] [reason]`", value="mutes a user in text channels", inline=False)
         embed.add_field(name="`?unmute [user]`", value="unmutes a user in text channels", inline=False)
         embed.add_field(name="`?warn [user] [reason]`", value="warns user (still working on system)", inline=False)
@@ -47,8 +47,8 @@ class helpcmd(Cog):
             title="Xea Fun Commands",
             colour=0xe86823
         )
-        bot_url=client.user.avatar_url
-        embed.set_thumbnail(url=bot_url)
+        # bot_url=client.user.avatar_url
+        # embed.set_thumbnail(url=bot_url)
         embed.add_field(name="`?hug [user]`", value="hugs the specified user", inline=False)
         embed.add_field(name="`?spongebob [text]`", value="turns the given text into sPoNGebOB cAsE", inline=False)
         embed.add_field(name="`?avatar {user}`", value="will get the avatar of specified user, if left empty gets authors avatar", inline=False)
@@ -56,14 +56,14 @@ class helpcmd(Cog):
         embed.add_field(name="`?clap [text]`", value="this will add clap emojis to your text", inline=False)
         await ctx.send(embed=embed)
 
-    @help.command()
+    @help.command(aliases=["poll", "q"])
     async def polls(self, ctx):
         embed = discord.Embed(
             title="Xea Poll Commands",
             colour=0xe86823
         )
-        bot_url=client.user.avatar_url
-        embed.set_thumbnail(url=bot_url)
+        # bot_url=client.user.avatar_url
+        # embed.set_thumbnail(url=bot_url)
         embed.add_field(name="`?poll [question]`", value="will make a yes, no, \'shrug\' poll", inline=False)
         embed.add_field(name="`?pollx \"[question in quotes]\" \"Option one\" \"Option two\" \"Option three\"`", value="gives you server icon", inline=False)
         await ctx.send(embed=embed)
@@ -74,8 +74,8 @@ class helpcmd(Cog):
             title="Xea Info Commands",
             colour=0xe86823
         )
-        bot_url=client.user.avatar_url
-        embed.set_thumbnail(url=bot_url)
+        # bot_url=client.user.avatar_url
+        # embed.set_thumbnail(url=bot_url)
         embed.add_field(name="`?userinfo {user}`", value="gives you info about the user", inline=False)
         embed.add_field(name="`?icon`", value="gives you server icon", inline=False)
         await ctx.send(embed=embed)
@@ -86,8 +86,8 @@ class helpcmd(Cog):
             title="Xea Integration Commands",
             colour=0xe86823
         )
-        bot_url=client.user.avatar_url
-        embed.set_thumbnail(url=bot_url)
+        # bot_url=client.user.avatar_url
+        # embed.set_thumbnail(url=bot_url)
         embed.add_field(name="`?r {subreddit} {index of post}`", value="returns post from reddit. Remeber index starts at 0", inline=False)
         await ctx.send(embed=embed)
 
